330249373c0f0458b1ffe66f6599192e2de1f5e6
Joey Turnbull <joeyjturnbull@gmail.com>
2025-06-16 16:26:20 -0700

    Fixed wording

--- plotting/target_fn_plotting.py
+++ plotting/target_fn_plotting.py
@@ -70,7 +70,9 @@ def plot_v_tildes(all_v_tildes, monomials, axes=None, fig=None, titles=None, sup
             ax.set_yscale("log")
             ax.text(0.7, 0.9, f'$\\Sigma \\tilde{{v}}^2 = {v_tilde_sum:.2e}$', **text_kwargs)
             if v_true is not None:
-                v_true_clipped = transform_fn(v_true)[:v_tildes.shape[0]].cpu().numpy()
+                clipper = kwargs.get("clipper", None)
+                clipper = clipper[i] if clipper is not None else v_tildes.shape[0]
+                v_true_clipped = transform_fn(v_true)[:clipper].cpu().numpy()
                 ax.plot(np.linspace(1, v_tildes.shape[0]+1, v_tildes.shape[0]), v_true_clipped, color='xkcd:black', zorder=10)
                 ax.text(0.7, 0.8, f'$\\Sigma \\tilde{{v_{{True}}}}^2 = {v_true_clipped.sum():.2e}$', **text_kwargs)
 
@@ -89,7 +91,9 @@ def plot_v_tildes(all_v_tildes, monomials, axes=None, fig=None, titles=None, sup
                 plt.scatter(indices[idxs], avg_v[idxs], color=colors[degree%7], linestyle='', marker='.', alpha=1/(degree+1),)
             
         if v_true is not None:
-            v_true_clipped = transform_fn(v_true)[:all_v_tildes.shape[0]].cpu().numpy()
+            clipper = kwargs.get("clipper", None)
+            clipper = clipper[i] if clipper is not None else v_tildes.shape[0]
+            v_true_clipped = transform_fn(v_true)[:clipper].cpu().numpy()
             plt.plot(np.linspace(1, v_tildes.shape[0]+1, v_tildes.shape[0]), v_true_clipped, color='xkcd:black', zorder=10)
             plt.text(0.7, 0.8, f'$\\Sigma \\tilde{{v_{{True}}}}^2 = {v_true_clipped.sum():.2e}$', **{'fontsize': 12})
             
