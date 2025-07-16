7ed4698135b4a98f31bdf5fb6050251ba0459961
Dhruva Karkada <dkarkada@gmail.com>
2025-07-16 00:23:13 +0000

    add Hyperparam manager

--- utils.py
+++ utils.py
@@ -1,5 +1,39 @@
 import numpy as np
 import torch
+from dataclasses import dataclass, field, asdict
+import json
+import base64
+
+
+@dataclass(frozen=True)
+class Hyperparams:
+    expt_name: str
+    dataset: str
+    kernel_name: str
+    kernel_width: float     = 4
+    n_samples: int          = 20_000
+    p_modes: int            = 10_000
+    # If using synth data, set these
+    data_dim: int           = 200
+    data_eigval_exp: float  = 1.2
+    beta: float             = 1.1
+    noise_var: float        = 0.
+    # If using natural image data, set these
+    zca_strength: float     = 5e-3
+    
+    def save(self, filepath):
+        """Save hyperparameters to JSON file"""
+        with open(filepath, 'w') as f:
+            json.dump(asdict(self), f, indent=4)
+    
+    def generate_filepath(self):
+        fp = f"{self.kernel_name}-kw:{self.kernel_width}"
+        if self.dataset == "gaussian":
+            fp += f"-data:{self.data_dim}:{self.data_eigval_exp}:{self.beta}:{self.noise_var:.3f}"
+        else:
+            fp += f"-zca:{self.zca_strength}"
+        return fp
+
 
 def ensure_numpy(x):
     """Convert torch.Tensor to numpy array if necessary."""
