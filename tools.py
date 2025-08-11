478085559e46c57cfe0b98c80166b1165841f659
Joey Turnbull <joeyjturnbull@gmail.com>
2025-08-11 15:55:14 -0700

    Updated find_beta for slope

--- tools.py
+++ tools.py
@@ -24,7 +24,7 @@ def get_standard_tools(X, kerneltype, kernel_width, top_mode_idx=3000, data_eigv
 
 
 def find_beta(K, y, num_estimators=20, n_test=100, n_trials=20, rng=np.random.default_rng(42), **kwargs):
-    sizes = np.logspace(0, np.log10(K.shape[0]-n_test)-0.2, num=num_estimators)
+    sizes = np.logspace(np.log10(kwargs.get("size_start", 1)), np.log10(K.shape[0]-n_test)-kwargs.get('size_offset', 0.2), num=num_estimators)
     K = ensure_torch(K)
     y = ensure_torch(y)
     test_mses = np.zeros((n_trials, num_estimators))
@@ -40,10 +40,13 @@ def find_beta(K, y, num_estimators=20, n_test=100, n_trials=20, rng=np.random.de
 
             test_mses[trial, i] = test_mse
             torch.cuda.empty_cache()
-        
-    sizes_w_intercept = torch.stack([ensure_torch(np.log10(sizes)), torch.ones_like(ensure_torch(sizes))], dim=1)   # [M, 2]  (M ≤ D after masking)
-    sol = torch.linalg.lstsq(sizes_w_intercept, ensure_torch(np.log10(test_mses.mean(axis=0))).unsqueeze(1)).solution.squeeze()
-    slope, intercept = sol[0], sol[1]
+
+    log_sizes = torch.log10(ensure_torch(sizes))
+    log_sizes_centered_w_intercept = torch.column_stack((log_sizes, torch.ones_like(log_sizes)))
+
+    sol = torch.linalg.lstsq(log_sizes_centered_w_intercept, ensure_torch(torch.log10(ensure_torch(test_mses).mean(axis=0))).unsqueeze(1)).solution.squeeze()
+    slope          = float(sol[0])
+    intercept     = float(sol[1])
     beta = -slope+1
 
     return beta, intercept, sizes, test_mses
