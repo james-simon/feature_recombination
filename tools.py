32a8a7de077eaa49addff85f53aad08e037e0f0f
Joey Turnbull <joeyjturnbull@gmail.com>
2025-08-11 17:56:47 -0700

    Beta bug fix

--- tools.py
+++ tools.py
@@ -34,7 +34,7 @@ def find_beta(K, y, num_estimators=20, n_test=100, n_trials=20, rng=np.random.de
             train_pool = np.arange(0, n)
             train_idx  = rng.permutation(train_pool)[:n]
             test_idx   = np.arange(K.shape[0] - n_test, K.shape[0])
-            idxs       = np.concatenate([train_idx, test_idx])
+            idxs       = ensure_torch(np.concatenate([train_idx, test_idx]))
             K_sub, y_sub = K[idxs[:, None], idxs[None, :]], y[idxs]
             (y_hat_test, y_test), _ = krr(K_sub, y_sub, n_train=n, n_test=n_test, ridge=kwargs.get("ridge", 1e-20))
             
