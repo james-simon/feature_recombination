815f626cad0e5e8de1e73c8e396251491e5f3601
Joey Turnbull <joeyjturnbull@gmail.com>
2025-08-13 17:34:48 -0700

    Fixed get_test_mses

--- tools.py
+++ tools.py
@@ -23,14 +23,16 @@ def get_standard_tools(X, kerneltype, kernel_width, top_mode_idx=3000, data_eigv
     return monomials, kernel, H, fra_eigvals, data_eigvals
 
 def get_test_mses(K, y, num_estimators=20, n_test=100, n_trials=20, **kwargs):
-    sizes = np.logspace(np.log10(kwargs.get("size_start", 100)), np.log10(K.shape[0]-n_test)-kwargs.get('size_offset', 0.01), num=num_estimators)
+    sizes = kwargs.get('sizes', None)
+    if sizes is None:
+        sizes = np.logspace(np.log10(kwargs.get("size_start", 100)), np.log10(K.shape[0]-n_test)-kwargs.get('size_offset', 0.01), num=num_estimators)
     K = ensure_torch(K)
     y = ensure_torch(y)
     test_mses = np.zeros((n_trials, num_estimators))
     for i, n in enumerate(sizes):
         n = int(n)
         for trial in range(n_trials):
-            train_idx = torch.arange(0, n)
+            train_idx = torch.arange(0, K.shape[0] - n_test)
             test_idx   = torch.arange(K.shape[0] - n_test, K.shape[0])
             idxs       = torch.concatenate([train_idx, test_idx]).to(K.device)
             K_sub, y_sub = K[idxs[:, None], idxs[None, :]], y[idxs]
