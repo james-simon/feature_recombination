5bbafa8c27bf4049d983dd1e4683e56633ff831f
Dhruva Karkada <dkarkada@gmail.com>
2025-08-21 15:40:27 +0000

    removed hyperparams class

--- utils.py
+++ utils.py
@@ -1,35 +1,5 @@
 import numpy as np
 import torch
-from dataclasses import dataclass, asdict
-import json
-
-
-@dataclass
-class Hyperparams:
-    expt_name: str
-    dataset: str
-    kernel_name: str
-    kernel_width: float     = 4
-    n_samples: int          = 20_000
-    p_modes: int            = 10_000
-    # If using synth data, set these
-    data_dim: int           = 200
-    data_eigval_exp: float  = 1.2
-    # If using natural image data, set these
-    zca_strength: float     = 5e-3
-    
-    def save(self, filepath):
-        """Save hyperparameters to JSON file"""
-        with open(filepath, 'w') as f:
-            json.dump(asdict(self), f, indent=4)
-    
-    def generate_filepath(self):
-        fp = f"{self.kernel_name}-kw:{self.kernel_width}"
-        if self.dataset == "gaussian":
-            fp += f"-data:{self.data_dim}:{self.data_eigval_exp}"
-        else:
-            fp += f"-zca:{self.zca_strength}"
-        return fp
 
 
 def ensure_numpy(x):
