6d7648988bd600bab1381af024f5f7407cf108cf
Dhruva Karkada <dkarkada@gmail.com>
2025-08-28 20:39:56 +0000

    optionally force clone

--- utils.py
+++ utils.py
@@ -2,16 +2,24 @@ import numpy as np
 import torch
 
 
-def ensure_numpy(x, dtype=np.float64):
-    """Convert torch.Tensor to numpy array if necessary."""
+def ensure_numpy(x, dtype=np.float64, clone=False):
+    """Convert torch.Tensor to numpy array if necessary.
+    """
     if isinstance(x, torch.Tensor):
         return x.detach().cpu().numpy()
-    return np.asarray(x, dtype=dtype)
+    result = np.asarray(x, dtype=dtype)
+    if clone and result is x:
+        return result.copy()
+    return result
 
 
-def ensure_torch(x, dtype=torch.float32):
-    """Convert numpy array to torch.Tensor if needed, and ensure correct dtype."""
+def ensure_torch(x, dtype=torch.float32, clone=False):
+    """Convert numpy array to torch.Tensor if needed, and ensure correct dtype.
+    """
     DEVICE = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     if not isinstance(x, torch.Tensor):
         return torch.tensor(x, dtype=dtype, device=DEVICE)
-    return x.to(dtype=dtype, device=DEVICE)
+    result = x.to(dtype=dtype, device=DEVICE)
+    if clone and result is x:
+        return result.clone()
+    return result
