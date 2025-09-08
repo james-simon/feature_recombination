d8838f3f2421f76e2c631f3aca34bfbcfe743f3d
dhruva <dkarkada@berkeley.edu>
2025-09-08 03:32:08 -0700

    clean kappa calc

--- eigenlearning.py
+++ eigenlearning.py
@@ -3,16 +3,11 @@ from utils import ensure_numpy
 
 
 def compute_learnabilities(n, eigvals, ridge=0):
-
-    def solve_kappa(n, eigvals, ridge):
-        eigvals = ensure_numpy(eigvals)
-        conservation_law = lambda kap: (eigvals/(eigvals+kap)).sum() + ridge/kap - n
-        kappa = scipy.optimize.bisect(conservation_law, 1e-25, 1e10, maxiter=128)
-        return kappa
-
-    kappa = solve_kappa(n, eigvals, ridge)
+    eigvals = ensure_numpy(eigvals)
+    conservation_law = lambda kap: (eigvals/(eigvals+kap)).sum() + ridge/kap - n
+    kappa = scipy.optimize.bisect(conservation_law, 1e-25, 1e10, maxiter=128)
     learnabilities = eigvals / (eigvals + kappa)
-    return kappa, learnabilities
+    return learnabilities, kappa
 
 
 def learning_curve(n, learnabilities, eigcoeffs, noise_var=0):
@@ -24,7 +19,7 @@ def learning_curve(n, learnabilities, eigcoeffs, noise_var=0):
 
 
 def eigenlearning(n, eigvals, eigcoeffs, ridge=0, noise_var=0):
-    kappa, learnabilities = compute_learnabilities(n, eigvals, ridge)
+    learnabilities, kappa = compute_learnabilities(n, eigvals, ridge)
     test_mse = learning_curve(n, learnabilities, eigcoeffs, noise_var)
     train_mse = (ridge / (n * kappa))**2 * test_mse
     L = (eigcoeffs**2 * learnabilities).sum() / (eigcoeffs**2).sum()
