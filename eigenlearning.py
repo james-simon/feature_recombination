9af0185e401247c2480a1f365847ff5b2e43e2f0
dhruva <dkarkada@berkeley.edu>
2025-09-08 03:27:59 -0700

    factored eigenlearning calcs for modularity

--- eigenlearning.py
+++ eigenlearning.py
@@ -1,7 +1,8 @@
 import scipy
 from utils import ensure_numpy
 
-def eigenlearning(n, eigvals, eigcoeffs, ridge=0, noise_var=0):
+
+def compute_learnabilities(n, eigvals, ridge=0):
 
     def solve_kappa(n, eigvals, ridge):
         eigvals = ensure_numpy(eigvals)
@@ -10,18 +11,27 @@ def eigenlearning(n, eigvals, eigcoeffs, ridge=0, noise_var=0):
         return kappa
 
     kappa = solve_kappa(n, eigvals, ridge)
-    if len(eigcoeffs) < len(eigvals):
-        eigvals = eigvals[:len(eigcoeffs)]
     learnabilities = eigvals / (eigvals + kappa)
+    return kappa, learnabilities
+
+
+def learning_curve(n, learnabilities, eigcoeffs, noise_var=0):
+    if len(eigcoeffs) < len(learnabilities):
+        learnabilities = learnabilities[:len(eigcoeffs)]
     e0 = n / (n - (learnabilities**2).sum())
     test_mse = e0 * (((1-learnabilities)**2 * eigcoeffs**2).sum() + noise_var)
+    return test_mse
+
+
+def eigenlearning(n, eigvals, eigcoeffs, ridge=0, noise_var=0):
+    kappa, learnabilities = compute_learnabilities(n, eigvals, ridge)
+    test_mse = learning_curve(n, learnabilities, eigcoeffs, noise_var)
     train_mse = (ridge / (n * kappa))**2 * test_mse
     L = (eigcoeffs**2 * learnabilities).sum() / (eigcoeffs**2).sum()
 
     return {
         "kappa": kappa,
         "learnabilities": learnabilities,
-        "overfitting_coeff": e0,
         "target_learnability": L,
         "train_mse": train_mse,
         "test_mse": test_mse,
