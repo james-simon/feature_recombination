ae46e5cfd3bf1eafefc3762bc43b6161471add19
dhruva <dkarkada@berkeley.edu>
2025-09-10 07:54:26 -0700

    iterating on monomial learning curve expts

--- scripts/eiglrn_monomial.sh
+++ scripts/eiglrn_monomial.sh
@@ -6,5 +6,5 @@
 # Expt 4 "Laplace Kernel @ CIFAR10"
 
 for i in {1..4}; do
-  CUDA_VISIBLE_DEVICES=$i nohup uv run python -u expts/hehe_eigenlearning.py $i monomials > "nohup$i.out" 2>&1 &
+  CUDA_VISIBLE_DEVICES=$i nohup uv run python -u expts/learning_curves.py $i monomials > "nohup$i.out" 2>&1 &
 done
